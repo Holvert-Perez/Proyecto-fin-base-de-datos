74ac5a89b2a3ada328fe16eff1fb590d7d3270b8
Holvert Perez <perezhol13@gmail.com>
2019-10-19 15:06:21 -0600

    2
    segundo commit

--- menu/My Project/Application.Designer.vb
+++ menu/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.menu.Form1
+            Me.MainForm = Global.menu.Login
         End Sub
     End Class
 End Namespace
